75279412a89a5d7eed2a42ced6101c165af4d274
Lucas Bonnet <lucasbonnet688@gmail.com>
2025-07-13 20:44:22 -0300

    Implementação da camada de Serviço

---
+++ src/main/java/me/dio/service/UserServImpl.java
@@ -0,0 +1,29 @@
+package me.dio.service;
+
+import java.util.NoSuchElementException;
+
+import me.dio.domain.model.User;
+import me.dio.domain.repository.UserRepository;
+
+public class UserServImpl implements UserService{
+
+	private final UserRepository userRepository;
+	
+	public UserServImpl(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+	
+	@Override
+	public User findById(Long id) {
+		return userRepository.findById(id).orElseThrow(NoSuchElementException::new);
+	}
+
+	@Override
+	public User create(User user) {
+		if(user != null && userRepository.existsById(user.getId())) {
+			throw new IllegalArgumentException("This user already exists.");
+		}
+		return userRepository.save(user);
+	}
+
+}
